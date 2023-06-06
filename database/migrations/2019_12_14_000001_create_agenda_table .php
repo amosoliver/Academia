f20dccf2043b0313aa1e3d9725b763d9fcb1c0a9
amosoliver <oliveramos13542@gmail.com>
2023-06-05 22:00:47 -0400

    wip

---
+++ database/migrations/2019_12_14_000001_create_agenda_table .php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgendaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agenda', function (Blueprint $table) {
+            $table->id();
+            $table->date('data');
+            $table->time('hora');
+            $table->unsignedBigInteger('id_aluno');
+            $table->foreign('id_aluno')->references('id')->on('alunos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agenda');
+    }
+}
